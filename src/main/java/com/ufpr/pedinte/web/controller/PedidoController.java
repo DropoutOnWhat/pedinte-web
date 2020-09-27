b7af480904555e62126233e83978b2ff41077940
Reggie <regisgaboardi@gmail.com>
2020-09-27 14:44:20 -0300

    Iniciado feature do Pedido

---
+++ src/main/java/com/ufpr/pedinte/web/controller/PedidoController.java
@@ -0,0 +1,6 @@
+package com.ufpr.pedinte.web.controller;/**
+ * Created by Regis Gaboardi (@gmail.com)
+ * Provided with Love and IntelliJ IDEA for pedinte-spring.
+ * 27/09/2020
+ */public class PedidoController {
+}
---
+++ src/main/java/com/ufpr/pedinte/web/service/PedidoService.java
@@ -0,0 +1,6 @@
+package com.ufpr.pedinte.web.service;/**
+ * Created by Regis Gaboardi (@gmail.com)
+ * Provided with Love and IntelliJ IDEA for pedinte-spring.
+ * 27/09/2020
+ */public class PedidoService {
+}
